bb933791d48d45a167a9aa9cb75d33e66371d077
jlamai <jibril.lamai@gmail.com>
2022-01-04 23:55:37 -0500

    2nd Commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def function1():
-    print("Hello World")
-function1()
+class Robot:
+    def __init__(self, mode, wheels, size, tires,):
+        self.mode = mode  # mode whether land, sea, or sir
+        self.wheels = wheels  # Does it use wheels or does it fly
+        self.size = size  # What is the size of the vehicle
+        self.tires = tires  # Amount of tires the vehicle has
+
+    mode = input("Enter the mode of transportation:")
+    print("You chose " + mode)
+
+
+
+
+
+
+
+
+
